8b66ef8de753ae628db9fb0fdb1f3f673c8dfa3c
kalyani <kalyani.bagale@synerzip.com>
2016-04-19 19:07:50 +0530

    API call for signup and login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,15 +3,30 @@ module Api
         class UsersController < ApplicationController
 
             def signup
-                @user = User.new(user_params)
-                render json: @user
+                user = User.new(user_params)
+                user.save
+                render json: user
+            end
+
+
+            def login
+                if(user_params[:first_name] == 'Kalyani')
+                    user = User.find_by(first_name: 'Kalyani')
+                    render json: user
+                else
+                    payload = {
+                        message: "Bad Credentials",
+                        status: 400
+                    }
+                    render :json => payload, :status => :bad_request
+                end
             end
 
 
             private
 
                 def user_params
-                    params.require(:user).permit(:first_name, :last_name)
+                    params.require(:user).permit(:first_name, :last_name, :email, :address, :phone_number)
                 end
 
         end
